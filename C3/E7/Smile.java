dff52394f4dee96076cb1e06d207f53592558633
S. M. Chase <smchase@protonmail.com>
2019-09-17 15:01:40 -0400

    started c4

---
+++ C3/E7/Smile.java
@@ -0,0 +1,7 @@
+public class Smile
+{
+    public static void main(String[] args)
+    {
+        System.out.println("  0   0\n    ^\n   \\_/\n");
+    }
+}
---
+++ C4/E1/ObjectHeight.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ObjectHeight
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        double time;
+
+        System.out.println("Enter a time less than 4.5 seconds: ");
+        time = input.nextDouble();
+
+        System.out.printf("\nThe height of the object is: %d\n", 100–4.9*time^2);
+    }
+}
