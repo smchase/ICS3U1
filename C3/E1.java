802708a51ca3885233762eee554978e6b9e813db
S. M. Chase <smchase@protonmail.com>
2019-09-17 14:43:21 -0400

    E1 and E2

--- C3/E1.java
+++
@@ -1,9 +0,0 @@
-package e1;
-
-public class E1
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Hello world!");
-    }
-}
---
+++ C3/E2/BingoRules.java
@@ -0,0 +1,7 @@
+public class BingoRules
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Bingo Card\n\n1. The caller randomly pulls a numbered bingo ball.\n\n2. The number is placed on the bingo board and called out.\n\n3. Players look for the called number on their bingo card.\n\n4. If the number is located, it is marked off.\n\n5. Steps 1 to 4 are repeated until a player matches the BINGO pattern.\n\n6. The winning player yells BINGO.\n\n");
+    }
+}
