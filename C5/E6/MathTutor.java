5269ef227272a088700e3998785938e1ee9e0cd8
Simon <smchase@protonmail.com>
2019-09-22 21:19:15 -0400

    progress

--- C5/E6/MathTutor.java
+++ C5/E6/MathTutor.java
@@ -4,15 +4,49 @@ public class MathTutor
 {
     public static void main(String[] args)
     {
-        int num1 = (int)(10*Math.random() + 1), num2 = (int)(10*Math.random() + 1), operator = (int)(4*Math.random() + 1);
+        int num1 = (int)(10*Math.random() + 1), num2 = (int)(10*Math.random() + 1), operator = (int)(4*Math.random()), answer;
         Scanner input = new Scanner(System.in);
-        System.out.printf("What is %d + %d? ", num1, num2);
-        int answer = input.nextInt();
-        if (answer == num1+num2) {
-            System.out.println("Correct!");
-        } else {
-            System.out.println("Incorrect!");
-        }
-        if (operator == 0)
+
+        switch (operator) {
+			case 0:
+				System.out.printf("What is %d + %d? ", num1, num2);
+		        answer = input.nextInt();
+		        if (answer == num1+num2) {
+		            System.out.println("Correct!");
+		        } else {
+		            System.out.println("Incorrect!");
+		        }
+				break;
+
+			case 1:
+				System.out.printf("What is %d - %d? ", num1, num2);
+		        answer = input.nextInt();
+		        if (answer == num1-num2) {
+		            System.out.println("Correct!");
+		        } else {
+		            System.out.println("Incorrect!");
+		        }
+				break;
+
+			case 2:
+				System.out.printf("What is %d * %d? ", num1, num2);
+		        answer = input.nextInt();
+		        if (answer == num1*num2) {
+		            System.out.println("Correct!");
+		        } else {
+		            System.out.println("Incorrect!");
+		        }
+				break;
+
+			case 3:
+				System.out.printf("What is %d / %d? ", num1, num2);
+		        answer = input.nextInt();
+		        if (answer == num1/num2) {
+		            System.out.println("Correct!");
+		        } else {
+		            System.out.println("Incorrect!");
+		        }
+				break;
+		}
     }
 }
