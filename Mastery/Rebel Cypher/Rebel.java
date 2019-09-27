2fefdce84ce4cd54e5d8b63116b619b027464d52
S. M. Chase <smchase@protonmail.com>
2019-09-27 15:04:48 -0400

    started rebel

---
+++ Mastery/Rebel Cypher/Rebel.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Rebel
+{
+    public static void main(String[] args) throws Exception
+    {
+        Scanner input = new Scanner(new File("data.txt"));
+        String decoded = input.nextLine(), encoded = input.nextLine(), message = input.nextLine();
+
+        char[95] map;
+        for (int i = 0; i < decoded.length(); i++) {
+            
+        }
+    }
+
+}
