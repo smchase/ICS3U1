0191a3fb643780da1177fe93ffee241a370fc39b
Simon <smchase@protonmail.com>
2019-09-28 21:28:32 -0400

    finished rebel cypher

--- Mastery/Rebel Cypher/Rebel.java
+++ Mastery/Rebel Cypher/Rebel.java
@@ -1,5 +1,5 @@
-import java.io.File;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Rebel
 {
@@ -8,10 +8,15 @@ public class Rebel
         Scanner input = new Scanner(new File("data.txt"));
         String decoded = input.nextLine(), encoded = input.nextLine(), message = input.nextLine();
 
-        char[95] map;
+        char[] map = new char[95];
+		Arrays.fill(map, '.');
         for (int i = 0; i < decoded.length(); i++) {
-            
+            map[(int)encoded.charAt(i)-32] = decoded.charAt(i);
         }
-    }
 
+		for (int i = 0; i < message.length(); i++) {
+			System.out.print(map[(int)message.charAt(i)-32]);
+		}
+		System.out.println();
+    }
 }
